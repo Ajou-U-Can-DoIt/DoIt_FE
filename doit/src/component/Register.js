75e5f4265a48dcba50047f24a8ff2ec2c0d20630
yu04038 <yu04038@gmail.com>
2021-07-05 20:25:14 +0900

    회원가입 css 추가

--- doit/src/component/Register.js
+++ doit/src/component/Register.js
@@ -1,110 +1,117 @@
 import React from 'react';
 import useForm from './useForm';
 import validate from './validateInfo';
-import "./Form.css";
+import "./Register.css";
 
 const Register = ( {submitForm } ) => {
     const {handleChange, values, handleSubmit, errors} = useForm(submitForm, validate);
     return (
-        <div className='form-content-right'>
-            <form className='form' onSubmit={handleSubmit}>
-                <h1>회원가입 화면</h1>
-            <div className="form-inputs">
-                <label htmlFor="username"
-                className="form-label">
-                    이름
-                </label>
-                <br></br>
-                <input
-                    id="username"
+        <div className='form-content-left'>
+            <div className="left">
+                <form className='form' onSubmit={handleSubmit}>
+                    <div className="logo">
+                        <h1>Do-it</h1>
+                    </div>
+                    <div className="title">
+                        <h1>Sign in</h1>
+                    </div>
+                <div className="form-inputs">
+                    <label htmlFor="username"
+                    className="form-label">
+                        이름
+                    </label>
+                    <input
+                        id="username"
+                        type="text"
+                        name="username"
+                        className="form-input"
+                        placeholder="이름 입력"
+                        value={values.username}
+                        onChange={handleChange}
+                    />
+                    {errors.username && <p>{errors.username}</p>}
+                </div>
+                <div className="form-inputs">
+                    <label htmlFor="id"
+                    className="form-label">
+                        아이디
+                    </label>
+                    <input
+                    id="id"
                     type="text"
-                    name="username"
+                    name="id"
                     className="form-input"
-                    placeholder="이름 입력"
-                    value={values.username}
+                    placeholder="아이디 입력"
+                    value={values.id}
                     onChange={handleChange}
-                />
-                {errors.username && <p>{errors.username}</p>}
-            </div>
-            <div className="form-inputs">
-                <label htmlFor="id"
-                className="form-label">
-                    아이디
-                </label>
-                <br></br>
-                <input
-                id="id"
-                type="text"
-                name="id"
-                className="form-input"
-                placeholder="아이디 입력"
-                value={values.id}
-                onChange={handleChange}
-                />
-                {errors.id && <p>{errors.id}</p>}
-            </div>
-            <div className="form-inputs">
-                <label htmlFor="password"
-                className="form-label">
-                    비밀 번호      
-                </label>
-                <br></br>
-                <input
-                    id="password"
-                    type="password"
-                    name="password"
+                    />
+                    {errors.id && <p>{errors.id}</p>}
+                </div>
+                <div className="form-inputs">
+                    <label htmlFor="password"
+                    className="form-label">
+                        비밀 번호      
+                    </label>
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        className="form-input"
+                        placeholder="비밀 번호 입력"
+                        value={values.password}
+                        onChange={handleChange}
+                    />
+                    {errors.password && <p>{errors.password}</p>}
+                </div>
+                <div className="form-inputs">
+                    <label htmlFor="password2"
+                    className="form-label" >
+                        비밀 번호(확인)
+                    </label>
+                    <input
+                        id="password2"
+                        type="password"
+                        name="password2"
+                        className="form-input"
+                        placeholder="비밀 번호 입력"
+                        value={values.password2}
+                        onChange={handleChange}
+                    />
+                    {errors.password2 && <p>{errors.password2}</p>}
+                </div>
+                <div className="form-inputs" f>
+                    <label htmlFor="email"
+                    className="form-label">
+                        이메일
+                    </label>
+                    <input
+                    id="email"
+                    type="email"
+                    name="email"
                     className="form-input"
-                    placeholder="비밀 번호 입력"
-                    value={values.password}
+                    placeholder="이메일 입력"
+                    value={values.email}
                     onChange={handleChange}
-                />
-                {errors.password && <p>{errors.password}</p>}
+                    />
+                    {errors.email && <p>{errors.email}</p>}
+                </div>
+                <br></br>
+                    <button className="form-input-btn"
+                    type="submit">
+                        회원 가입
+                    </button>
+                    <br></br>
+                    <br></br>
+                    <span className="form-input-register">
+                        이미 계정이 있으신가요? <a href="/login">로그인</a>
+                    </span>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                </form>
             </div>
-            <div className="form-inputs">
-                <label htmlFor="password2"
-                className="form-label" >
-                    비밀 번호(확인)
-                </label>
-                <br></br>
-                <input
-                    id="password2"
-                    type="password"
-                    name="password2"
-                    className="form-input"
-                    placeholder="비밀 번호 입력"
-                    value={values.password2}
-                    onChange={handleChange}
-                />
-                {errors.password2 && <p>{errors.password2}</p>}
-            </div>
-            <div className="form-inputs" f>
-                <label htmlFor="email"
-                className="form-label">
-                    이메일
-                </label>
-                <br></br>
-                <input
-                id="email"
-                type="email"
-                name="email"
-                className="form-input"
-                placeholder="이메일 입력"
-                value={values.email}
-                onChange={handleChange}
-                />
-                {errors.email && <p>{errors.email}</p>}
-            </div>
-            <br></br>
-                <button className="form-input-btn"
-                type="submit">
-                    회원 가입
-                </button>
-                <br></br>
-                <br></br>
-                <span className="form-input-register">
-                    이미 계정이 있으신가요? <a href="/login">로그인</a>
-                </span>
-            </form>
         </div>
     );
 }
--- doit/src/component/validateInfo.js
+++ doit/src/component/validateInfo.js
@@ -2,32 +2,32 @@ export default function validateInfo(values) {
     let errors = {}
 
     if(!values.username.trim()) {
-        errors.username = "Username is required";
+        errors.username = "이름을 입력해주세요.";
     }
 
     if(!values.id.trim()) {
-        errors.id = "ID is required";
+        errors.id = "ID를 입력해주세요.";
     } else if(values.id.length < 6) {
-        errors.id = "ID needs to be 6 characters or more"
+        errors.id = "ID는 6자리 이상이어야 합니다."
     }
 
     //Email
     if(!values.email) {
-        errors.email = "Email required";
+        errors.email = "이메일을 입력해주세요.";
     } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-        errors.email = "Email address is invalid";
+        errors.email = "유효하지 않은 이메일 양식입니다.";
     }
 
     if(!values.password) {
-        errors.password = "Password is required";
+        errors.password = "비밀번호를 입력해주세요.";
     } else if(values.password.length < 6){
-        errors.password = "Password needs to be 6 characters or more";
+        errors.password = "비밀번호는 6자리 이상이어야 합니다.";
     }
 
     if (!values.password2) {
-        errors.password2 = "Password is required";
+        errors.password2 = "비밀번호를 입력해주세요.";
     } else if (values.password2 !== values.password) {
-        errors.password2 = "Password do not match";
+        errors.password2 = "비밀번호가 맞지 않습니다.";
     }
 
     return errors;
