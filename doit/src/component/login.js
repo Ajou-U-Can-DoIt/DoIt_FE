566bedc07b2f70869eb2b5fa93dbd9fe9c7365b3
yu04038 <yu04038@gmail.com>
2021-07-14 21:35:58 +0900

    login 구현

--- doit/src/component/login.js
+++ doit/src/component/login.js
@@ -1,52 +1,67 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./login.css";
 
 const Login = ( { history } ) => {
 
     return (
-        <div className="form-content-right">
-            <form className="form">
-                <h3> 로그인 화면 </h3>
-                <div className="form-inputs">
-                    <label htmlFor="ID" className="form-label">
-                        아이디
-                    </label>
+        <div className="form-content-left">
+            <div className="left">
+                <form className="form">
+                    <div className="logo">
+                        <h1>Do-it</h1>
+                    </div>
+                    <div className="title">
+                        <h1>Login</h1>
+                        <br></br>
+                        <br></br>
+                    </div>
+                    <div className="login-form-inputs">
+                        <label htmlFor="ID" 
+                        className="login-form-label">
+                            아이디
+                        </label>
+                        <input 
+                            id="ID"
+                            type="text" 
+                            name="ID" 
+                            className='login-form-input'
+                            placeholder="아이디를 입력하세요"
+                            />
+                    </div>
                     <br></br>
-                    <input 
-                        id="ID"
-                        type="text" 
-                        name="ID" 
-                        className='form-input'
-                        placeholder="Enter your ID"
-                        />
-                </div>
-                <br></br>
-                <div className="form-inputs">
-                    <label htmlFor="password" 
-                    className="form-label">
-                        비밀번호
-                    </label>
+                    <div className="login-form-inputs">
+                        <label htmlFor="password" 
+                        className="login-form-label"
+                        >
+                            비밀번호
+                        </label>
+                        <input 
+                            id="password"
+                            type="password" 
+                            name="password" 
+                            className='login-form-input'
+                            placeholder="비밀번호를 입력하세요"
+                            />
+                    </div>
+                    <div className="auto-login">
+                        <input type="checkbox"/><span>&nbsp;로그인 상태 유지</span>
+                    </div>
                     <br></br>
-                    <input 
-                        id="password"
-                        type="password" 
-                        name="password" 
-                        className='form-input'
-                        placeholder="Enter your Password"
-                        />
-                </div>
-                <br></br>
-                <Link to="./main">
-                    <button>
-                        로그인
-                    </button>
-                </Link>
-                <br></br>
-                <br></br>
-                <span className="form-input-register">
-                    계정이 없으신가요? <a href="/register">여기를 눌러 생성하세요</a>
-                </span>
-            </form>
+                    <br></br>
+
+                    <Link to="./main">
+                        <button className="login-btn">
+                            로그인
+                        </button>
+                    </Link>
+                    <br></br>
+                    <br></br>
+                    <span className="form-input-register">
+                        계정이 없으신가요? <a href="/register">여기를 눌러 생성하세요</a>
+                    </span>
+                </form>
+            </div>      
         </div>
     );
 }
