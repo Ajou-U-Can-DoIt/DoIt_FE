4cc4c653b930bf013b88d8e54aa4db234fd63af6
yu04038 <yu04038@gmail.com>
2021-07-17 19:26:27 +0900

    메인화면 달력 추가

--- doit/src/component/main.js
+++ doit/src/component/main.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import {Calendar, momentLocalizer} from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./main.css"
 
-const Main = ( { history } ) => {
+const localizer = momentLocalizer(moment);
 
+const myEventList = [{ start: new Date(), end: new Date(), title: "special event"}];
+
+export default function Main() {
     return (
-        <div>
-            <h3> Main 화면! 여기다 구현하면 됩니당 </h3>
-        </div>
+        <body>
+            <div className="calendar">
+                <Calendar
+                    localizer={localizer}
+                    events={myEventList}
+                    startAccessor="start"
+                    endAccessor="end"
+                    style={{height: 700, width: 1000, display: "inline-block", float: "right"}}
+                    />
+                </div>
+        </body>
+        
     );
 }
 
-export default Main;
+
