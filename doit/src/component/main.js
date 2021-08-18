e43d5a3b03bb0f8ec73dd490e0895c2352450447
yu04038 <yu04038@gmail.com>
2021-08-18 09:32:03 +0900

    Update main.js

--- doit/src/component/main.js
+++ doit/src/component/main.js
@@ -3,7 +3,7 @@ import Calendar from '@toast-ui/react-calendar';
 import 'tui-calendar/dist/tui-calendar.css';
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
-import "./main.css"
+import "./main.css";
 import { Link} from 'react-router-dom';
 import Select from "react-select/creatable";
 
